2c155533bd49aa779f203571046979e86023d38e
devookim <devookim@gmail.com>
2024-05-06 22:58:16 +0900

    Rename and update comments tracking_object.py

--- src/picam_with_cv/tracking_object.py
+++ src/picam_with_cv/tracking_object.py
@@ -2,45 +2,42 @@ from pprint import pprint
 import cv2
 from picamera2 import Picamera2, Preview
 
-
-
+# Configure trackers
 trackers = [cv2.legacy.TrackerBoosting_create,
             cv2.legacy.TrackerMIL_create,
             cv2.legacy.TrackerKCF_create,
             cv2.legacy.TrackerTLD_create,
             cv2.legacy.TrackerMedianFlow_create,
-            #cv2.legacy.TrackerGOTURN_create, #버그로 오류 발생
+            #cv2.legacy.TrackerGOTURN_create, #Possibly deprecated
             cv2.legacy.TrackerCSRT_create,
             cv2.legacy.TrackerMOSSE_create]
 trackerIdx = 0
 tracker = None
 isFirst = True
+delay=1
+win_name = 'Tracking APIs'
 
 # Configure Picamera2
 picam2 = Picamera2()
 capture_config = picam2.create_still_configuration()
-picam2.configure(picam2.create_preview_configuration({"size": (640, 480)}))
-
 picam2.configure(capture_config)# Without the config, Unsupported foramt error is thrown at fgbg.apply()
 picam2.start()
-delay=1
-win_name = 'Tracking APIs'
-print('Processing frames')
 
+print('Processing frames')
 while True:
     frame = picam2.capture_array()
-    
     img_draw = frame.copy()
-    if tracker is None: # 트랙커 생성 안된 경우
+    if tracker is None: # ROI is unset initial step
         cv2.putText(img_draw, "Press the Space to set ROI!!", \
             (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2,cv2.LINE_AA)
-    else:
-        ok, bbox = tracker.update(frame)   # 새로운 프레임에서 추적 위치 찾기 ---③
+    else: # in case of tracker set
+        ok, bbox = tracker.update(frame)   # Update bbox
         (x,y,w,h) = bbox
-        if ok: # 추적 성공
+        if ok: # Draw a newly updated bounding box with rect
             cv2.rectangle(img_draw, (int(x), int(y)), (int(x + w), int(y + h)), \
                           (0,255,0), 2, 1)
-        else : # 추적 실패
+            print('tracking')
+        else : # On a tracking fail
             cv2.putText(img_draw, "Tracking fail.", (100,80), \
                         cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2,cv2.LINE_AA)
     trackerName = tracker.__class__.__name__
@@ -49,17 +46,19 @@ while True:
 
     cv2.imshow(win_name, img_draw)
     key = cv2.waitKey(delay) & 0xff
-    # 스페이스 바 또는 비디오 파일 최초 실행 ---④
-    if key == ord(' ') or ( isFirst): 
+    # Wait key input and configure tracker
+    if key == ord(' ') or isFirst: #Select ROI and set default tracker
         isFirst = False
-        roi = cv2.selectROI(win_name, frame, False)  # 초기 객체 위치 설정
-        if roi[2] and roi[3]:         # 위치 설정 값 있는 경우
-            tracker = trackers[trackerIdx]()    #트랙커 객체 생성 ---⑤
+        roi = cv2.selectROI(win_name, frame, False)  # Drag an area of ROI
+        print('Select ROI')
+        if roi[2] and roi[3]:         # when roi is set (area), initialize tracker with the ROI
+            tracker = trackers[trackerIdx]()
             isInit = tracker.init(frame, roi)
-    elif key in range(48, 56): # 0~7 숫자 입력   ---⑥
-        trackerIdx = key-48     # 선택한 숫자로 트랙커 인덱스 수정
+            print('tracker is initalized')
+    elif key in range(48, 55): #key (0~6) change tracker and init with the existing bounding box
+        trackerIdx = key-48 
         if bbox is not None:
-            tracker = trackers[trackerIdx]() # 선택한 숫자의 트랙커 객체 생성 ---⑦
-            isInit = tracker.init(frame, bbox) # 이전 추적 위치로 추적 위치 초기화
-    elif key == 27 : 
+            tracker = trackers[trackerIdx]()
+            isInit = tracker.init(frame, bbox)
+    elif key == 27 : #ESC for exit 
         break
