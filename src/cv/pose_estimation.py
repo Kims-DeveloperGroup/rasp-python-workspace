a047be382d12ffaacee08d8805eac33b729dc19b
rica-v3 <rica@dunamu.com>
2024-05-17 22:33:42 +0900

    cv/pose_esitimation write pose data

--- src/cv/pose_estimation.py
+++ src/cv/pose_estimation.py
@@ -4,33 +4,32 @@ from cv2 import VideoCapture
 import matplotlib.pyplot as plt
 import numpy as np
 import mediapipe as mp
+import csv
 
-#BaseOptions = mp.tasks.BaseOptions
-#PoseLandmarker = mp.tasks.vision.PoseLandmarker
-#PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
-#VisionRunningMode = mp.tasks.vision.RunningMode
+def write_landmarks_to_csv(landmarks, csv_data, frame_number, writer):
+    print(f"Landmark coordinates for frame {frame_number}:")
+    for idx, landmark in enumerate(landmarks):
+        print(f"{mp_pose.PoseLandmark(idx).name}: (x: {landmark.x}, y: {landmark.y}, z: {landmark.z})")
+        writer.writerow([frame_number, idx, landmark.x, landmark.y, landmark.z])
+    print("\n")
 
-# Create a pose landmarker instance with the video mode:
-#options = PoseLandmarkerOptions(
-#    base_options=BaseOptions(model_asset_path=model_path),
-#    running_mode=VisionRunningMode.VIDEO)
-
+# init data.csv
+data_file_path = '/Users/rica/Documents/data.csv' 
+file = open(data_file_path, 'w', newline='')
+csv.writer(file).writerow(['frame', 'pose', 'x', 'y', 'z'])
+file.close()
+file = open(data_file_path, 'a', newline='')
+writer = csv.writer(file)
 # Initialize MediaPipe Pose and Drawing utilities
 mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
 pose = mp_pose.Pose()
 
-def write_landmarks_to_csv(landmarks, csv_data, frame_number):
-    print(f"Landmark coordinates for frame {frame_number}:")
-    for idx, landmark in enumerate(landmarks):
-        print(f"{mp_pose.PoseLandmark(idx).name}: (x: {landmark.x}, y: {landmark.y}, z: {landmark.z})")
-        csv_data.append([frame_number, mp_pose.PoseLandmark(idx).name, landmark.x, landmark.y, landmark.z])
-    print("\n")
-
-
 frame_number = -1
 csv_data = []
 video = cv2.VideoCapture(0)
+
+# Read frames
 while video.isOpened():
 	ret, frame = video.read()
 	if not ret:
@@ -44,10 +43,13 @@ while video.isOpened():
 	# Draw the pose landmarks on the frame
 	if result.pose_landmarks:
 	    mp_drawing.draw_landmarks(frame_rgb, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-	    write_landmarks_to_csv(result.pose_landmarks.landmark, csv_data, frame_number)
+	    write_landmarks_to_csv(result.pose_landmarks.landmark, csv_data, frame_number, writer)
     # Display the frame
 	cv2.imshow('MediaPipe Pose', frame_rgb)
 	if cv2.waitKey(1) & 0xff ==27:
 		break
+
+# Release open resources
+file.close()
 video.release()
 cv2.destroyAllWindows()
