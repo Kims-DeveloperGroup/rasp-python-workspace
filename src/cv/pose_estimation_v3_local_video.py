1abe88cd61141faf7c9004c073d51154092fe8d4
rica-v3 <rica@dunamu.com>
2024-06-07 13:54:13 +0900

    pose_est_v3_local make function run

--- src/cv/pose_estimation_v3_local_video.py
+++ src/cv/pose_estimation_v3_local_video.py
@@ -33,7 +33,6 @@ def write_landmarks_to_csv(landmarks, label):
 	print("\n")
 	csv_data.append(label)
 	return csv_data
-timer = 0
 def getTime():
 	return time.time()
 def create_file_writer(path):
@@ -49,76 +48,78 @@ def create_file_writer(path):
 	print(column_names)
 	writer.writerow(column_names)
 	return (writer, file)
-data_file_path = '/Users/rica/Documents/data_v3.csv' 
-writer, file = create_file_writer(data_file_path)
-
-#Text Config
-rgb = (255, 0, 0)
-font = cv2.FONT_HERSHEY_SIMPLEX
-video_src = 0
-# Capture frames
-frame_rgb = None
-video = VideoCapture(video_src)
-label = -1
-count = 0
-started = False
-capture_duration = 2 # Time unit:sec
-# Key guide
-# Space: Stop a video for labeling
-while video.isOpened():
-	# Read frmes from video
-	ret, frame = video.read()
-	if not ret:
-		break
-	if frame_rgb is not None:
-		cv2.imshow('MediaPipe Pose', frame_rgb)
-	frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-	result = pose.process(frame_rgb)
-	mp_drawing.draw_landmarks(frame_rgb, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-
-	# Wait a key input
-	key = cv2.waitKey(1) & 0xff
+def run():
+	data_file_path = '/Users/rica/Documents/data_v3.csv' 
+	writer, file = create_file_writer(data_file_path)
 	
-	# Exit the loop
-	if key == 27:
-		break
-	elif started == False and key != ord(' '):
-		cv2.putText(frame_rgb, 'READY', (50, 500), font, 15.0, rgb, 50)
-	elif started == False and key == ord(' '):
-		started = True
-		cv2.putText(frame_rgb, 'START', (50, 500), font, 15.0, rgb, 50)
-	elif count != 0 and count % 10 == 0 : # At dvery 10 dataset, decide to continue or not
-		cv2.putText(frame_rgb, 'CONTINUE \nOR NOT', (50, 500), font, 5.0, rgb, 10)
-		#key = cv2.waitKey(-1) & 0xff
-		if key == 27:# Stop
+	#Text Config
+	rgb = (255, 0, 0)
+	font = cv2.FONT_HERSHEY_SIMPLEX
+	video_src = 0
+	# Capture frames
+	frame_rgb = None
+	video = VideoCapture(video_src)
+	label = -1
+	timer = 0
+	count = 0
+	started = False
+	capture_duration = 2 # Time unit:sec
+	# Key guide
+	# Space: Stop a video for labeling
+	while video.isOpened():
+		# Read frmes from video
+		ret, frame = video.read()
+		if not ret:
 			break
-		else : # Continue
+		if frame_rgb is not None:
+			cv2.imshow('MediaPipe Pose', frame_rgb)
+		frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		result = pose.process(frame_rgb)
+		mp_drawing.draw_landmarks(frame_rgb, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+	
+		# Wait a key input
+		key = cv2.waitKey(1) & 0xff
+		
+		# Exit the loop
+		if key == 27:
+			break
+		elif started == False and key != ord(' '):
+			cv2.putText(frame_rgb, 'READY', (50, 500), font, 15.0, rgb, 50)
+		elif started == False and key == ord(' '):
+			started = True
+			cv2.putText(frame_rgb, 'START', (50, 500), font, 15.0, rgb, 50)
+		elif count != 0 and count % 10 == 0 : # At dvery 10 dataset, decide to continue or not
+			cv2.putText(frame_rgb, 'CONTINUE \nOR NOT', (50, 500), font, 5.0, rgb, 10)
+			#key = cv2.waitKey(-1) & 0xff
+			if key == 27:# Stop
+				break
+			else : # Continue
+				timer = 0
+		# Lable frames and write in a csv file
+		elif started == True and label == -1 and timer == 0: 
+			label = random_label()
+			labelText = labels[label]
+			print(f'label={label} value:{labelText}')
+			timer = getTime()
+		elif label != -1 and timer != 0 and (getTime() - timer) > capture_duration:
+			cv2.putText(frame_rgb, "CAPTURE", (50,50), font, 3.0, rgb, 10)
+			# Get csv data from a frame
+			#result = pose.process(frame_rgb)
+			if result.pose_landmarks is not None:
+				#mp_drawing.draw_landmarks(frame_rgb, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+				csv_data = write_landmarks_to_csv(result.pose_landmarks.landmark, label)
+				#csv_data.append(label)
+				writer.writerow(csv_data) # Write a csv file
+			label = -1
 			timer = 0
-	# Lable frames and write in a csv file
-	elif started == True and label == -1 and timer == 0: 
-		label = random_label()
-		labelText = labels[label]
-		print(f'label={label} value:{labelText}')
-		timer = getTime()
-	elif label != -1 and timer != 0 and (getTime() - timer) > capture_duration:
-		cv2.putText(frame_rgb, "CAPTURE", (50,50), font, 3.0, rgb, 10)
-		# Get csv data from a frame
-		#result = pose.process(frame_rgb)
-		if result.pose_landmarks is not None:
-			#mp_drawing.draw_landmarks(frame_rgb, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-			csv_data = write_landmarks_to_csv(result.pose_landmarks.landmark, label)
-			#csv_data.append(label)
-			writer.writerow(csv_data) # Write a csv file
-		label = -1
-		timer = 0
-		count+=1
-	else :
-		cv2.putText(frame_rgb, f"{time.time() - timer} sec", (50,50), font, 2.0, rgb, 2)
-		cv2.putText(frame_rgb, f"{count}:{labels[label]}", (50, 500), font, 10.0, rgb, 50)
-# Release open resources
-file.close()
-video.release()
-cv2.destroyAllWindows()
-
-model_path = 'tf/models/boxing_pose_est_v1.keras'
-train(data_file_path, model_path)
+			count+=1
+		else :
+			cv2.putText(frame_rgb, f"{time.time() - timer} sec", (50,50), font, 2.0, rgb, 2)
+			cv2.putText(frame_rgb, f"{count}:{labels[label]}", (50, 500), font, 10.0, rgb, 50)
+	# Release open resources
+	file.close()
+	video.release()
+	cv2.destroyAllWindows()
+	
+	model_path = 'tf/models/boxing_pose_est_v1.keras'
+	train(data_file_path, model_path)
