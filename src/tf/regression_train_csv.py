0071f2f9ad7b5828904be44e954351b01708d84a
rica-v3 <rica@dunamu.com>
2024-06-07 13:55:14 +0900

    regress_train_csv reuse existing model

--- src/tf/regression_train_csv.py
+++ src/tf/regression_train_csv.py
@@ -3,35 +3,43 @@ import numpy as np
 
 # Make numpy values easier to read.
 np.set_printoptions(precision=3, suppress=True)
-
+import os
+import time
 import tensorflow as tf
 from tensorflow.keras import layers
+def train(dataset_path, model_path):
+	# Load csv data and make features and labels
+	model = None
+	try:
+		model = tf.keras.models.load_model(model_path)
+		print(f'The model is loaded in {model_path}')
+	except:
+		print(f'Newly creates in {model_path}')
+	dataset = pd.read_csv(dataset_path)
+	labels = np.array(dataset.pop("label"))
+	features = np.array(dataset)
+	print(f'label={labels}\n')
+	print(f'features={features}\n')
+	
+	#Preprocessing dataset
+	normalizer = layers.Normalization()
+	normalizer.adapt(features.copy())
+	
+	# Make regression model
+	if model is None:
+		model = tf.keras.Sequential([
+	  	normalizer,
+	  	layers.Dense(64, activation='relu'),
+	  	layers.Dense(1)
+		])
+	
+		model.compile(loss = tf.keras.losses.MeanSquaredError(),
+	                      optimizer = tf.keras.optimizers.Adam())
+	
+	# Train model
+	model.fit(features, labels, epochs=10)
+	model.save(model_path)
 
-# Load csv data and make features and labels
-path = '/Users/rica/Documents/data.csv'
-dataset = pd.read_csv(path)
-dataset.head()
-features = dataset.copy()
-labels = features.pop('label')
-
-print(features)
-print(labels)
-
-# Preprocessing dataset
-features = np.array(features)
-print(f'features={features}')
-normalizer = layers.Normalization()
-normalizer.adapt(features)
-
-# Make regression model
-model = tf.keras.Sequential([
-  normalizer,
-  layers.Dense(64, activation='relu'),
-  layers.Dense(1)
-])
-
-model.compile(loss = tf.keras.losses.MeanSquaredError(),
-                      optimizer = tf.keras.optimizers.Adam())
-# Train model
-model.fit(features, labels, epochs=10)
-model.save('tf/models/test_v1.keras')
+path = '/Users/rica/Documents/data_v3.csv'
+model_path = 'tf/models/test_v1.keras'
+#train(path, model_path)
