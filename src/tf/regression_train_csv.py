9e55c69b281b9871202989d45cb312d77539cd16
rica-v3 <rica@dunamu.com>
2024-06-30 19:18:45 +0900

    regression_train train with onehot_enc

--- src/tf/regression_train_csv.py
+++ src/tf/regression_train_csv.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 
-# Make numpy values easier to read.
-np.set_printoptions(precision=3, suppress=True)
 import os
 import time
 import tensorflow as tf
@@ -18,59 +16,63 @@ def train(dataset_path, model_path, epochs):
 	dataset = pd.read_csv(dataset_path)
 	labels = np.array(dataset.pop("label"))
 	features = np.array(dataset)
-	print(f'label={labels}\n')
-	print(f'features={features}\n')
-	
+	trained_model = _train(features, labels, epochs, model=model)
+	trained_model.save(model_path)
+	trained_model.summary()
+	return model
+def _train(features, labels, epochs= 10, model = None):
 	#Preprocessing dataset
 	normalizer = layers.Normalization()
 	normalizer.adapt(features.copy())
-	
+	onehot_enc = tf.one_hot(indices=labels, depth=10, dtype = tf.int32)
 	# Make regression model
 	if model is None:
 		model = tf.keras.Sequential([
+		layers.InputLayer(input_shape=(33 * 3,)),
 	  	normalizer,
 	  	layers.Dense(64, activation='relu'),
-		layers.Dense(64, activation='relu'),
-	  	layers.Dense(1)
+	  	layers.Dense(32, activation='relu'),
+	  	layers.Dense(16, activation='relu'),
+	  	#layers.Dense(10),
+		layers.Dense(10, activation='softmax'),
 		])
 	
 		model.compile(loss = tf.keras.losses.MeanAbsoluteError(),
 	                      optimizer = tf.keras.optimizers.Adam())
-	
 	# Train model
-	model.fit(features, labels, epochs=epochs)
-	model.save(model_path)
+	model.fit(features, onehot_enc, epochs=epochs)
 	return model
-
-path = '/Users/rica/Documents/data_v3.csv'
-model_path = 'tf/models/test_v1.keras'
-#train(path, model_path)
-
-def avg(lst): 
-    return sum(lst) / len(lst)
 
 def test(features, labels, model):
 	expected = labels
 	actual = model.predict(features)
-
-	actualValue = []
-	for value in actual:
-		actualValue.append(value[0])
-	print(f'expected={expected}\n')
-	print(f'actual={actualValue}\n')
-	
-	diff= []
-	for idx, value in enumerate(actualValue):
-		diff.append(abs(value - expected[idx]))
-	print(f'diff={diff} {avg(diff)}\n')
+	copied_features = features.copy()
+	error_feats = []
+	error_labels = []
+	for idx, value in enumerate(actual):
+		flag = ''
+		if expected[idx] != np.argmax(value):
+			flag = '[INCORRECT]'
+			error_feats.append(features[idx])
+			error_labels.append(expected[idx])
+			print(f'{flag} expected={expected[idx]} predicted={np.argmax(value)},\n values={value}')
+	return np.array(error_feats), np.array(error_labels)
 
 def loadFileAndTest(dataset_path, model_path):
 	model = tf.keras.models.load_model(model_path)
 	dataset = pd.read_csv(dataset_path)
 	expected = np.array(dataset.pop("label"))
 	features = np.array(dataset)
-	test(features, expected, model)	
+	error = test(features, expected, model)
+	model.summary()
+	#return error
 
 def trainAndTest(dataset_path, model_path, trainEpochs):
-	train(dataset_path, model_path, trainEpochs)
-	loadFileAndTest(dataset_path, model_path)
+	model = train(dataset_path, model_path, trainEpochs)
+	error_feats, error_labels = loadFileAndTest(dataset_path, model_path)
+	# Re-train with error features
+#	while len(error_feats) > 0:
+#		model = _train(error_feats, error_labels, trainEpochs, model=model)
+#		error_feats, error_labels = test(error_feats, error_labels, model)
+#		time.sleep(10)
+
