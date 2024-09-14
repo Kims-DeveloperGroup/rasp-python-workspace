cc0251c7ec8c98cf5ec2aaeab0f26a5ba4ede83b
rica-v3 <rica@dunamu.com>
2024-09-14 18:57:23 +0900

    normalize 함수 오류 수정

--- src/tf/regression_train_csv.py
+++ src/tf/regression_train_csv.py
@@ -9,17 +9,23 @@ from tensorflow.keras import layers
 labelNames = ['Jab', 'Straight', 'FR-Hook', 'BH-Hook', 'FR-Upper', 'BH-Upper', 'FR-Body', 'BH-Body    ', 'BodyJab', 'BodyStraight']
 
 def normalize(poses):
-	normalized_poses =  []
-	for pose in poses:
-		landmarks = pose
-		min_xyz = landmarks.argmin(axis=0)
-		normalized = landmarks.copy()
-		for landmark in landmarks:
-			normalized[0] = normalized[0] - landmarks[min_xyz[0]][0]	
-			normalized[1] = normalized[1] - landmarks[min_xyz[1]][1]	
-			normalized[2] = normalized[2] - landmarks[min_xyz[2]][2]
-		normalized_poses.append(normalized.tolist())
-	return normalized_poses 
+     normalized_poses =  []
+     for pose in poses:
+         landmarks = pose[11:]
+         min_xyz = landmarks.argmin(axis=0)
+         minX = landmarks[min_xyz[0]][0]
+         minY = landmarks[min_xyz[1]][1]
+         minZ = landmarks[min_xyz[2]][2]
+         normalized_landmarks = []
+         for landmark in landmarks:
+             normalized = landmark.copy()
+             #print(f'before={normalized}')
+             normalized[0] = landmark[0] - minX
+             normalized[1] = landmark[1] - minY
+             #print(f'after={normalized}')
+             normalized_landmarks.append(normalized.tolist())
+         normalized_poses.append(normalized_landmarks)
+     return np.array(normalized_poses)
 
 def train(dataset_path, model_path, epochs):
 	# Load csv data and make features and labels
@@ -36,32 +42,42 @@ def train(dataset_path, model_path, epochs):
 	trained_model.save(model_path)
 	trained_model.summary()
 	return model
+
 def _train(features, labels, epochs= 10, model = None):
-	#Preprocessing dataset
-	normalizer = layers.Normalization()
-	normalizer.adapt(features.copy())
+	features_copy = features.copy()
+	features_copy = np.reshape(features_copy, (-1, 33,3))
+	features_copy = normalize(features_copy)
 	onehot_enc = tf.one_hot(indices=labels, depth=10, dtype = tf.float32)
+
 	# Make regression model
 	if model is None:
 		model = tf.keras.Sequential([
-		layers.InputLayer(shape=(33 * 3,)),
-	  	normalizer,
-	  	#layers.Dense(99, activation='relu'),
-	  	#layers.Dense(32, activation='relu'),
-	  	layers.Dense(1024, activation='relu'),
-		layers.Dropout(rate=0.3),
+		layers.InputLayer(shape=(22,3)),
+	  	#normalizer,
+	  	layers.Flatten(),
+		layers.Dense(90, activation='relu'),
+		layers.Dropout(rate=0.2),
+	  	layers.Dense(45, activation='relu'),
+		layers.Dropout(rate=0.2),
 	  	layers.Dense(10, activation='softmax'),
-		#layers.Dense(10),
 		])
 	
 		model.compile(loss = tf.keras.losses.CategoricalCrossentropy(),
 	                      optimizer = tf.keras.optimizers.Adam(),
 						  metrics = [tf.keras.metrics.CategoricalAccuracy()])
 	# Train model
-	model.fit(features, onehot_enc, epochs=epochs)
+	model.fit(
+				x=features_copy,
+				y=onehot_enc,
+				epochs=epochs,
+				validation_split = 0.2,
+				batch_size = 40,
+			)
 	return model
 
 def test(features, labels, model):
+	features = np.reshape(features, (-1, 33,3))
+	features = normalize(features)
 	expected = labels
 	actual = model.predict(features)
 	loss, accuracy = model.evaluate(features, tf.one_hot(indices=labels, depth=10, dtype = tf.float32))
