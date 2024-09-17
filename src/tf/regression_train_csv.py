96ab6524c3f34815e740d0cb1eefd8a2eb62f086
rica-v3 <rica@dunamu.com>
2024-09-17 15:03:47 +0900

    loadAndTest normalize 파라미터 추가

--- src/tf/regression_train_csv.py
+++ src/tf/regression_train_csv.py
@@ -8,18 +8,19 @@ from tensorflow.keras import layers
 
 labelNames = ['Jab', 'Straight', 'FR-Hook', 'BH-Hook', 'FR-Upper', 'BH-Upper', 'FR-Body', 'BH-Body    ', 'BodyJab', 'BodyStraight']
 
-def normalize(poses, truncate_feat=True):
+def _normalize(poses, truncate_feat=True):
      poses=poses.reshape((-1, 33,3))
      normalized_poses =  []
      for pose in poses:
+         landmarks = None     
          if truncate_feat == True:
              landmarks = pose[11:]
          else:
              landmarks = pose
-         min_xyz = landmarks.argmin(axis=0)
-         minX = landmarks[min_xyz[0]][0]
-         minY = landmarks[min_xyz[1]][1]
-         minZ = landmarks[min_xyz[2]][2]
+         #11 left shoulder 
+         minX = landmarks[11][0]
+         minY = landmarks[11][1]
+         minZ = landmarks[11][2]
          normalized_landmarks = []
          for landmark in landmarks:
              normalized = landmark.copy()
@@ -40,7 +41,7 @@ def normalize_file(source, dest, truncate_feat=False):
     feats = np.array(dataset).reshape((-1, 33,3))
     
     normalized_feats = []
-    for feat in normalize(feats, truncate_feat):
+    for feat in _normalize(feats, truncate_feat):
         normalized_feats.append(feat.flatten())
     normalized_feats = np.array(normalized_feats)
     if truncate_feat == True:
@@ -49,7 +50,7 @@ def normalize_file(source, dest, truncate_feat=False):
     dp.insert(loc=0, column='label', value=labels)
     dp.to_csv(path_or_buf=dest, mode='w', index=False)
 
-def train(dataset_path, model_path, epochs):
+def train(dataset_path, model_path, epochs, normalize=False):
 	# Load csv data and make features and labels
 	model = None
 	try:
@@ -60,22 +61,29 @@ def train(dataset_path, model_path, epochs):
 	dataset = pd.read_csv(dataset_path)
 	labels = np.array(dataset.pop("label"))
 	features = np.array(dataset)
-	trained_model = _train(features, labels, epochs, model=model)
+	trained_model = _train(features, labels, epochs, model=model, normalize=normalize)
 	trained_model.save(model_path)
 	trained_model.summary()
 	return model
 
-def _train(features, labels, epochs= 10, model = None):
+def _train(features, labels, epochs= 10, model = None, normalize=False):
 	features_copy = features.copy()
-	features_copy = normalize(features_copy)
+	if normalize == True:
+		features_copy= _normalize(poses= features_copy)
+	else:
+		features_copy= features_copy.reshape((-1, 33,3))
 	onehot_enc = tf.one_hot(indices=labels, depth=10, dtype = tf.float32)
+	
+	normalizer = layers.Normalization(axis=-1)
+	normalizer.adapt(features_copy)
 
 	# Make regression model
 	if model is None:
 		model = tf.keras.Sequential([
-		layers.InputLayer(shape=(22,3)),
-	  	#normalizer,
+		#layers.InputLayer(shape=(33,3)),
+	  	normalizer,
 	  	layers.Flatten(),
+		layers.Dense(189, activation='relu'),
 		layers.Dense(90, activation='relu'),
 		layers.Dropout(rate=0.2),
 	  	layers.Dense(45, activation='relu'),
@@ -96,8 +104,11 @@ def _train(features, labels, epochs= 10, model = None):
 			)
 	return model
 
-def test(features, labels, model):
-	features = normalize(features)
+def _test(features, labels, model, normalize):
+	if normalize == True:
+		features = _normalize(features)
+	else:
+		features = np.reshape(features, (-1, 33, 3))
 	expected = labels
 	actual = model.predict(features)
 	loss, accuracy = model.evaluate(features, tf.one_hot(indices=labels, depth=10, dtype = tf.float32))
@@ -114,12 +125,12 @@ def test(features, labels, model):
 	print(f'total={len(features)} errors={len(error_feats)}')
 	return np.array(error_feats), np.array(error_labels)
 
-def loadFileAndTest(dataset_path, model_path):
+def loadFileAndTest(dataset_path, model_path, normalize= False):
 	model = tf.keras.models.load_model(model_path)
-	dataset = pd.read_csv(dataset_path)
+	dataset = pd.read_csv(dataset_path).sort_values(by='label', axis= 0)
 	expected = np.array(dataset.pop("label"))
 	features = np.array(dataset)
-	error = test(features, expected, model)
+	error = _test(features, expected, model, normalize)
 	model.summary()
 	#return error
 
