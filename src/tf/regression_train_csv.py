8d81bd1f4443f09d87fe55dd081cf47c06e51b50
rica-v3 <rica@dunamu.com>
2024-09-20 15:55:27 +0900

    TrainCallback, active_landmark_indices 추가

--- src/tf/regression_train_csv.py
+++ src/tf/regression_train_csv.py
@@ -8,6 +8,8 @@ from tensorflow.keras import layers
 
 labelNames = ['Jab', 'Straight', 'FR-Hook', 'BH-Hook', 'FR-Upper', 'BH-Upper', 'FR-Body', 'BH-Body    ', 'BodyJab', 'BodyStraight']
 
+active_landmark_indices = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+
 def _normalize(poses, truncate_feat=True):
      poses=poses.reshape((-1, 33,3))
      normalized_poses =  []
@@ -50,7 +52,7 @@ def normalize_file(source, dest, truncate_feat=False):
     dp.insert(loc=0, column='label', value=labels)
     dp.to_csv(path_or_buf=dest, mode='w', index=False)
 
-def train(dataset_path, model_path, epochs, normalize=False):
+def train(dataset_path, model_path, epochs, test_dataset_path):
 	# Load csv data and make features and labels
 	model = None
 	try:
@@ -61,19 +63,33 @@ def train(dataset_path, model_path, epochs, normalize=False):
 	dataset = pd.read_csv(dataset_path)
 	labels = np.array(dataset.pop("label"))
 	features = np.array(dataset)
-	trained_model = _train(features, labels, epochs, model=model, normalize=normalize)
+	test_feats, test_labels = _loadDataset(test_dataset_path)
+	trained_model = _train(features, labels, test_features= test_feats, test_labels=test_labels, epochs=epochs, model=model)
 	trained_model.save(model_path)
 	trained_model.summary()
 	return model
 
-def _train(features, labels, epochs= 10, model = None, normalize=False):
-	features_copy = features.copy()
-	if normalize == True:
-		features_copy= _normalize(poses= features_copy)
-	else:
-		features_copy= features_copy.reshape((-1, 33,3))
+globalModel = None
+
+class TrainCallback(tf.keras.callbacks.Callback):
+	def __init__(self, m, f, l):
+		self.m = m
+		self.f = f
+		self.l = l
+
+	def on_epoch_end(self, epoch, logs={}):
+		loss, accuracy = self.m.evaluate(self.f, tf.one_hot(indices=self.l, depth=10, dtype = tf.float32))
+		print(f'accuracy={accuracy}')
+		if(accuracy >0.80):
+			print(f'logs={logs}')
+			self.model.stop_training = True
+
+def _train(features, labels, test_features, test_labels, epochs= 10, model = None):
+	features_copy= features.copy().reshape((-1, 33,3))
+	test_features= test_features.reshape((-1, 33,3))
 	onehot_enc = tf.one_hot(indices=labels, depth=10, dtype = tf.float32)
 	
+	features_copy = features_copy.take(active_landmark_indices, axis=1)
 	normalizer = layers.Normalization(axis=-1)
 	normalizer.adapt(features_copy)
 
@@ -83,33 +99,35 @@ def _train(features, labels, epochs= 10, model = None, normalize=False):
 		#layers.InputLayer(shape=(33,3)),
 	  	normalizer,
 	  	layers.Flatten(),
-		layers.Dense(189, activation='relu'),
-		layers.Dense(90, activation='relu'),
-		layers.Dropout(rate=0.2),
-	  	layers.Dense(45, activation='relu'),
+		#layers.Dense(189, activation='relu'),
+		#layers.Dense(90, activation='relu'),
+		#layers.Dropout(rate=0.2),
+	  	layers.Dense(10, activation='relu'),
 		layers.Dropout(rate=0.2),
 	  	layers.Dense(10, activation='softmax'),
 		])
+		globalModel= model
 	
 		model.compile(loss = tf.keras.losses.CategoricalCrossentropy(),
 	                      optimizer = tf.keras.optimizers.Adam(),
 						  metrics = [tf.keras.metrics.CategoricalAccuracy()])
 	# Train model
+
+	globalModel = model		
 	model.fit(
 				x=features_copy,
 				y=onehot_enc,
 				epochs=epochs,
 				validation_split = 0.2,
-				batch_size = 40,
+				batch_size = 20,
+				callbacks= TrainCallback(model, test_features, test_labels)
 			)
 	return model
 
-def _test(features, labels, model, normalize):
-	if normalize == True:
-		features = _normalize(features)
-	else:
-		features = np.reshape(features, (-1, 33, 3))
+def _test(features, labels, model):
+	features = np.reshape(features, (-1, 33, 3))
 	expected = labels
+	features= features.take(active_landmark_indices, axis=1)
 	actual = model.predict(features)
 	loss, accuracy = model.evaluate(features, tf.one_hot(indices=labels, depth=10, dtype = tf.float32))
 	copied_features = features.copy()
@@ -124,13 +142,16 @@ def _test(features, labels, model, normalize):
 			print(f'{flag} {idx}. expected={labelNames[expected[idx]]} predicted={labelNames[np.argmax(value)]},\n values={value}')
 	print(f'total={len(features)} errors={len(error_feats)}')
 	return np.array(error_feats), np.array(error_labels)
-
-def loadFileAndTest(dataset_path, model_path, normalize= False):
-	model = tf.keras.models.load_model(model_path)
-	dataset = pd.read_csv(dataset_path).sort_values(by='label', axis= 0)
-	expected = np.array(dataset.pop("label"))
+def _loadDataset(path):
+	dataset = pd.read_csv(path).sort_values(by='label', axis= 0)
+	labels = np.array(dataset.pop("label"))
 	features = np.array(dataset)
-	error = _test(features, expected, model, normalize)
+	return features, labels
+
+def loadFileAndTest(dataset_path, model_path):
+	model = tf.keras.models.load_model(model_path)
+	features, expected = _loadDataset(dataset_path)	
+	error = _test(features, expected, model)
 	model.summary()
 	#return error
 
